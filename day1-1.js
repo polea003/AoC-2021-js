fc225300aad6471c390f034e19ead7d15be77401
pwoleary <69978971+pwoleary@users.noreply.github.com>
2022-01-14 23:33:54 -0500

    day 1 part 2

--- day1-1.js
+++ day1-1.js
@@ -3,9 +3,17 @@ const fs = require('fs')
 
 var depths = fs.readFileSync('data/day1.data', 'utf8').split('\n')
 
+var sliding_windows = []
+
+const reducer = (previousValue, currentValue) => parseInt(previousValue) + parseInt(currentValue);
+
+for (var i = 0; i < depths.length - 2; i++) {
+    sliding_windows.push(depths.slice(i, i + 3).reduce(reducer))
+}
+
 var total_increases = 0
-for (var i = 1; i < depths.length; i++) {
-    if (parseInt(depths[i]) > parseInt(depths[i-1])) {
+for (var i = 1; i < sliding_windows.length; i++) {
+    if (parseInt(sliding_windows[i]) > parseInt(sliding_windows[i-1])) {
       total_increases++
     }
 }
